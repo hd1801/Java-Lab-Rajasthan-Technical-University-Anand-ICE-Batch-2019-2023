fc05b694b0acdcd87e7c048bf61445686706b67a
hd1801 <hardikdhawan0108@gmail.com>
2021-05-08 14:17:49 +0530

    8/5/21 lab 2

---
+++ IntTOString.java
@@ -0,0 +1,11 @@
+class IntTOString {
+    
+    public static void main(String args[])
+    {
+        int a=10;
+        System.out.println("integer value = " + a );
+        String x = String.valueOf(a);
+        System.out.println("String value = "+ x );
+    }
+    
+}
---
+++ StringTOint.java
@@ -0,0 +1,12 @@
+class StringtoInt
+{
+        
+    public static void main(String args[])
+    {
+        String x = "123";
+        System.out.println("String value = "+ x );
+        int a=Integer.parseInt(x);
+        System.out.println("integer value = " + a );
+    }
+    
+}
