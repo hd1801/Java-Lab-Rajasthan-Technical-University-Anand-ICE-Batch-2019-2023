dee5694fe13f5640f2b48f3c7e823fd024fefb5b
hd1801 <hardikdhawan0108@gmail.com>
2021-05-22 13:40:24 +0530

    lab 4 program 1 and 2

---
+++ lab/src/bike.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *lab 4 program 1 default constructor
+ * @author karti
+ */
+public class bike {
+    public bike()
+    {
+        System.out.println("a bike object has been created");
+    }
+    public static void main(String args[])
+    {
+        bike b=new bike();
+    }
+}
---
+++ lab/src/student.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * lab 4.2
+ * default constructor 2 
+ * @author karti
+ */
+public class student {
+   int id;
+   String name;
+    void Display()
+    {
+        System.out.println(id+" "+ name);
+    }
+    public static void main(String args[])
+    {
+        student s1= new student();
+        student s2= new student();
+        s1.Display();
+        s2.Display();
+    }
+    
+}
